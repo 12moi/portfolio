e678be6fcec7752cb57cdf7a03a6cec0c3da9137
12moi <moi.shadrack@student.moringaschool.com>
2022-05-15 11:46:06 +0300

    add contact elements

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import './Contact.css'
-export default function contact() {
+import { useState } from "react";
+export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
-    <div contactName='contact' id='contact'>contact</div>
-  )
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/work.png" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply ASAP :)</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
